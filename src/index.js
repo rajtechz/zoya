f2b4243b045d1bedbbd40be341a542e096b2b34f
triline <hiitechz07@gmail.com>
2023-04-29 18:37:14 +0530

    zoya project

--- src/index.js
+++ src/index.js
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { MantineProvider } from '@mantine/core';
+import { NavigationProgress } from '@mantine/nprogress';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MantineProvider theme={{
+      fontSizes: {
+        xxs: 9, xs: 14, sm: 16, md: 18, lg: 20, xl: 22, xxl: 25, lxl: 28, lxxl: 35
+      },
+
+      colors: {
+        basic: ["#0000", "#191919", "#37B24D",]
+      }
+    }} >
+      <NavigationProgress />
+      <App />
+    </MantineProvider>
   </React.StrictMode>
 );
 
